870d52d8ee7e300db1fbb6d5a5b0b4e76651bac2
Josakko <112590914+Josakko@users.noreply.github.com>
2025-06-12 01:17:47 +0200

    added something as symlink

--- 1razred/Algoritmi-i-programiranje/lv25/zad5.c
+++ 1razred/Algoritmi-i-programiranje/lv25/zad5.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-#include <ctype.h>
-
-
-void main() {
-    char a;
- 
-    do {
-        printf("Upisi veliko slovo: ");
-        scanf_s(" %c", &a);
-    } while (!isupper(a));
- 
-    printf("Slova od pocetka abecede do slova %c: ", a);
-    for (int i = 'A'; i < a; i++) 
-        printf("%c ", i);
-}
+./lv24/zad6.c
